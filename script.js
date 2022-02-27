2f3ca6a35b42454e7d34d394c7616762b278aec4
Tatyana <tatyezhova@yandex.ru>
2022-02-27 20:57:08 +0300

    add scripts

--- script.js
+++ script.js
@@ -3,28 +3,56 @@ const template = templateFragment.querySelector('div');
 const fragment = document.createDocumentFragment();
 const field = document.querySelector('.field');
 
+const moveFigure = (colorFigure, e) =>{
+  const x = e.target.dataset.x;
+  const y = e.target.dataset.y;
+
+  if(e.target.classList.contains('cell_active')) {
+    //удалить все фигуры которые уже стоят
+    allCell.map(el => {
+      if(el.classList.contains(`${colorFigure}`)) {
+        el.classList.remove(`${colorFigure}`, 'cell__figure')
+      }
+    })
+
+    //находим нужную клетку на доске и ставим новую фигуру
+    const newCellFigure = field.querySelector(`.cell[data-x="${x}"][data-y="${y}"]`);
+    newCellFigure.classList.add('cell__figure', `${colorFigure}`);
+    newCellFigure.addEventListener('click', getMove);
+  }
+
+    //удаляем все активные клетки, если кликнули мимо
+    allCell.map(cell => cell.classList.remove('cell_active'));
+}
 
 // просчитываем ходы
 const getMove = (event) => {
   const x = event.target.dataset.x;
   const y = event.target.dataset.y;
 
-  allCell = Array.from(document.getElementsByClassName('cell'));
+  // allCell = Array.from(document.getElementsByClassName('cell'));
 
   let cellX = allCell.filter(cell => cell.getAttribute('data-x') == x);
   let cellY = allCell.filter(cell => cell.getAttribute('data-y') == y);
 
-  allCell.map(cell => cell.classList.remove('cell_active'));
 
+  //удаляем все активные клетки, если кликнули мимо
+  allCell.map(cell => cell.classList.remove('cell_active'));
+  allCell.map(cell => cell.removeEventListener('click', moveFigure));
+  // allCell.map(cell => cell.removeEventListener('click', getMove));
+
+
+  const allCellActive = [];
 
   //если кликнули по ладье
-if(event.target.classList.contains('cell__figure_white')) {
+if(event.target.classList.contains('cell__figure')) {
+  const colorFigure = event.target.classList[3];
+
   //проверка на препядствие в одну сторону по X
   for(i=+y+1; i<8; i++){
-    console.log(i)
-    console.log(cellX[i])
-    if(!cellX[i].classList.contains('cell__figure_white')) {
+    if(!cellX[i].classList.contains('cell__figure')) {
       cellX[i].classList.add('cell_active');
+      allCellActive.push(cellX[i]);
     } else {
       break;
     }
@@ -32,8 +60,9 @@ if(event.target.classList.contains('cell__figure_white')) {
 
     //проверка на препядствие в другую сторону по X
   for(i=+y-1; i>=0; i--){
-    if(!cellX[i].classList.contains('cell__figure_white')) {
+    if(!cellX[i].classList.contains('cell__figure')) {
       cellX[i].classList.add('cell_active');
+      allCellActive.push(cellX[i]);
     } else {
       break;
     }
@@ -41,8 +70,9 @@ if(event.target.classList.contains('cell__figure_white')) {
 
     //проверка на препядствие в одну сторону по Y
   for(i=+x+1; i<8; i++) {
-    if(!cellY[i].classList.contains('cell__figure_white')) {
+    if(!cellY[i].classList.contains('cell__figure')) {
       cellY[i].classList.add('cell_active');
+      allCellActive.push(cellY[i]);
     } else {
       break;
     }
@@ -50,12 +80,16 @@ if(event.target.classList.contains('cell__figure_white')) {
 
     //проверка на препядствие в другую сторону по Y
   for(i=+x-1; i>=0; i--){
-    if(!cellY[i].classList.contains('cell__figure_white')) {
+    if(!cellY[i].classList.contains('cell__figure')) {
       cellY[i].classList.add('cell_active');
+      allCellActive.push(cellY[i]);
     } else {
       break;
     }
   }
+
+  //на все активные клетки вешаем обработчик клика
+  allCellActive.forEach(el => el.addEventListener('click', (e) => moveFigure(colorFigure, e)));
 }
 }
 
@@ -68,7 +102,6 @@ for(k=0; k<8; k++) {
     const element = template.cloneNode(true);
     element.setAttribute('data-x',`${k}`);
     element.setAttribute('data-y',`${i}`);
-    element.addEventListener('click', getMove);
     if(count%2 == 0){
       element.classList.add('cell_black');
     }
@@ -80,3 +113,14 @@ count++;
 field.appendChild(fragment);
 
 
+//стартовые позиции
+const figureWhite = field.querySelector('.cell[data-x="0"][data-y="2"]');
+figureWhite.classList.add('cell__figure', 'cell__figure_white');
+figureWhite.addEventListener('click', getMove);
+
+const figureBlack = field.querySelector('.cell[data-x="7"][data-y="5"]');
+figureBlack.classList.add('cell__figure', 'cell__figure_black');
+figureBlack.addEventListener('click', getMove);
+
+//все ячейки доски
+const allCell = Array.from(document.getElementsByClassName('cell'));
